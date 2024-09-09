8e540ff0a19afe4ef8484c99e6526ee7582ffef3
kashish00208 <kgupta7827@gmail.com>
2024-09-10 05:17:31 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const { API_KEY } = require("./config");
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
-const apiKey = process.env.API_KEY;
 app.get("/api/data", async (req, res) => {
   try {
-    const response = await axios.get(`https://gnews.io/api/v4/search?q=example&apikey=${apiKey}`);
+    const response = await axios.get(`https://gnews.io/api/v4/search?q=example&apikey=${API_KEY}`);
     res.json(response.data); 
   } catch (error) {
     console.error('Error fetching data from API:', error);
