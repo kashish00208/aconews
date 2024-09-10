272bf14c5456481475e3ab1cb50c1fa83ff9975d
kashish00208 <kgupta7827@gmail.com>
2024-09-10 06:15:05 +0530

    Changes

---
+++ frontend/components/HomePage.jsx
@@ -0,0 +1,40 @@
+// src/App.jsx
+import React, { useEffect, useState } from 'react';
+
+function HomePage() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/data');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div>
+      
+      <h1>Data from Backend:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default HomePage;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,12 @@
-// src/App.jsx
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import HomePage from "../components/HomePage";
 function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/api/data');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
-  return (
-    <div>
-      <h1>Data from Backend:</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+  return <>
+  <Navbar/>
+  <HomePage/>
+  </>
+  ;
 }
 
 export default App;
