28ffb6b0331e0a9ff08094e0840975c13a96f2d8
kashish00208 <kgupta7827@gmail.com>
2024-09-10 11:50:19 +0530

    changes

--- frontend/components/HomePage.jsx
+++ frontend/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function HomePage() {
   const [data, setData] = useState(null);
@@ -8,9 +8,9 @@ function HomePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/data');
+        const response = await fetch("http://localhost:5000/api/data");
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const result = await response.json();
         setData(result);
@@ -22,14 +22,48 @@ function HomePage() {
     };
 
     fetchData();
+    console.log(data)
   }, []);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div>
-      <h2>Stay Updated with Latest Insights</h2>
+    <div className="p-10 bg-slate-100 h-screen ">
+      <div className="flex justify-between">
+        <h2 className="text-2xl   m-4 font-black">Gnews</h2>
+        <h2 className="text-2xl   m-4 font-black">
+          Stay Updated with Latest Insights
+        </h2>
+      </div>
+
+      <div className="h-2/3 flex">
+        {/* <!-- Section 1 --> */}
+        <div className="w-1/2 flex flex-col">
+          <div className="flex-1 text-white flex items-center justify-center">
+            <img src="" alt="" />
+          </div>
+          <div className="flex-1 text-white flex items-center justify-center">
+            2
+          </div>
+        </div>
+
+        {/* <!-- Section 2 --> */}
+        <div className="w-1/2 flex flex-col">
+          <div className="flex-1 text-white flex items-center justify-center">
+            3
+          </div>
+          <div className="flex-1 flex">
+            <div className="w-1/2 text-white flex items-center justify-center">
+              4
+            </div>
+            <div className="w-1/2 text-whiteflex items-center justify-center">
+              5
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
     </div>
   );
