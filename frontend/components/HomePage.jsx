15b5d795311bcf24f90a8cf4d47bac5208a2af29
kashish00208 <kgupta7827@gmail.com>
2024-09-24 16:09:42 +0530

    Changes

--- frontend/components/HomePage.jsx
+++ frontend/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 function HomePage() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -12,34 +12,26 @@ function HomePage() {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        const result = await response.json();
-        setData(result);
+        const results = await response.json();
+        setData(results);
+        console.log(results)
       } catch (error) {
         setError(error.message);
       } finally {
         setLoading(false);
       }
     };
-
     fetchData();
-    console.log(data);
-    const article = data.articles;
-    console.log(article);
   }, []);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className="p-10 bg-slate-100 h-screen ">
-      <div className="flex justify-between">
-        <h2 className="text-2xl   m-4 font-black">Gnews</h2>
-        <h2 className="text-2xl   m-4 font-black">
-          Stay Updated with Latest Insights
-        </h2>
-      </div>
-      
-    </div>
+    <>
+    {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+    </>
+    
   );
 }
 
