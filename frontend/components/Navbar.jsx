803f3a3f53ffc56b4fb00b7187a6ad243ad28987
kashish00208 <kgupta7827@gmail.com>
2024-10-09 18:39:13 +0530

    minor changes

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className="text-2xl font-bold font-sans">Gnews</div>
         <ul className="flex flex-row space-x-4 md:space-x-6 list-none font-bold">
             <li><a className="block">Home</a></li>
-            <li><a className="block">Top'50 News</a></li>
+            <li><a className="block">Top'10 News</a></li>
         </ul>
       </nav>
   );
